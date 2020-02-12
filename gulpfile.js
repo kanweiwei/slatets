2e23dc1af872e33bc335b93ee544f648f7c1a68b
kanweiwei <kwwnjujlc@sina.com>
2020-02-12 18:20:17 +0800

    feat: publish slate-dev-environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,133 +1,148 @@
-const path = require("path")
+const path = require("path");
 const gulp = require("gulp");
-const rimraf = require("rimraf")
-const ts = require('gulp-typescript');
-const through2 = require('through2');
-const merge2 = require('merge2');
-const tsConfig = require('./getTsCommonConfig.js')();
-const replaceLib = require('./replaceLib');
-const babel = require('gulp-babel');
-const getBabelCommonConfig = require("./getBabelCommonConfig")
+const rimraf = require("rimraf");
+const ts = require("gulp-typescript");
+const through2 = require("through2");
+const merge2 = require("merge2");
+const tsConfig = require("./getTsCommonConfig.js")();
+const replaceLib = require("./replaceLib");
+const babel = require("gulp-babel");
+const getBabelCommonConfig = require("./getBabelCommonConfig");
 const tsDefaultReporter = ts.reporter.defaultReporter();
 
 const cwd = process.cwd();
 
-const stripCode = require('gulp-strip-code');
+const stripCode = require("gulp-strip-code");
 
-const rootDir = "packages"
+const rootDir = "packages";
 
 const entrys = [
-    "slate",
-    "slate-schema-violations",
-    "slate-hotkeys",
-    "slate-react",
-    "slate-base64-serializer",
-    "slate-html-serializer",
-    "slate-plain-serializer",
-    "slate-prop-types",
-]
+  "slate",
+  "slate-schema-violations",
+  "slate-hotkeys",
+  "slate-react",
+  "slate-base64-serializer",
+  "slate-html-serializer",
+  "slate-plain-serializer",
+  "slate-prop-types",
+  "slate-dev-environment"
+];
 
 function compileTs(stream) {
-    return stream
-        .pipe(ts(tsConfig)).js
-        .pipe(through2.obj(function (file, encoding, next) {
-            // console.log(file.path, file.base);
-            file.path = file.path.replace(/\.[jt]sx$/, '.js');
-            this.push(file);
-            next();
-        }))
+  return stream.pipe(ts(tsConfig)).js.pipe(
+    through2.obj(function(file, encoding, next) {
+      // console.log(file.path, file.base);
+      file.path = file.path.replace(/\.[jt]sx$/, ".js");
+      this.push(file);
+      next();
+    })
+  );
 }
 
 gulp.task("clean", () => {
-
-    rimraf.sync(path.join(cwd, "dist"))
-})
+  rimraf.sync(path.join(cwd, "dist"));
+});
 
 gulp.task("copyOtherFiles", ["clean"], () => {
-    let streams = entrys.map((entry) => {
-        let source = [
-            `${rootDir}/${entry}/package.json`,
-            `${rootDir}/${entry}/README.md`,
-            `${rootDir}/${entry}/LICENSE`
-        ];
-        return gulp.src(source).pipe(gulp.dest(`dist/${entry}`)).pipe(gulp.dest(`dist/${entry}`))
-            .pipe(gulp.dest(`dist/${entry}`))
+  let streams = entrys.map(entry => {
+    let source = [
+      `${rootDir}/${entry}/package.json`,
+      `${rootDir}/${entry}/README.md`,
+      `${rootDir}/${entry}/LICENSE`
+    ];
+    return gulp
+      .src(source)
+      .pipe(gulp.dest(`dist/${entry}`))
+      .pipe(gulp.dest(`dist/${entry}`))
+      .pipe(gulp.dest(`dist/${entry}`));
+  });
+  return merge2(streams);
+});
 
-    })
-    return merge2(streams)
-})
-
-gulp.task('tsc', ["copyOtherFiles", "clean"], () => {
-
-    let streams = entrys.map((entry) => {
-        let source = [
-            `${rootDir}/${entry}/src/**/*.ts`,
-            `${rootDir}/${entry}/src/**/*.tsx`,
-            '!node_modules/**/*.*',
-            `!${rootDir}/${entry}/node_modules/**/*.*`,
-            'typings/**/*.d.ts',
-        ];
-        return merge2(compileTs(gulp.src(source)).pipe(gulp.dest(`dist/${entry}/dist`)));
-
-    })
-    return merge2(streams)
+gulp.task("tsc", ["copyOtherFiles", "clean"], () => {
+  let streams = entrys.map(entry => {
+    let source = [
+      `${rootDir}/${entry}/src/**/*.ts`,
+      `${rootDir}/${entry}/src/**/*.tsx`,
+      "!node_modules/**/*.*",
+      `!${rootDir}/${entry}/node_modules/**/*.*`,
+      "typings/**/*.d.ts"
+    ];
+    return merge2(
+      compileTs(gulp.src(source)).pipe(gulp.dest(`dist/${entry}/dist`))
+    );
+  });
+  return merge2(streams);
 });
 
 function babelify(js, modules, entry) {
-    const babelConfig = getBabelCommonConfig(modules);
-    delete babelConfig.cacheDirectory;
-    if (modules === false) {
-        babelConfig.plugins.push(replaceLib);
-    } else {
-        babelConfig.plugins.push(require.resolve('babel-plugin-add-module-exports'));
-    }
-    let stream = js.pipe(babel(babelConfig))
-    if (modules === false) {
-        stream = stream.pipe(stripCode({
-            start_comment: '@remove-on-es-build-begin',
-            end_comment: '@remove-on-es-build-end',
-        }));
-    }
-    return stream.pipe(gulp.dest(modules === false ? `dist/${entry}/es` : `dist/${entry}/lib`));
+  const babelConfig = getBabelCommonConfig(modules);
+  delete babelConfig.cacheDirectory;
+  if (modules === false) {
+    babelConfig.plugins.push(replaceLib);
+  } else {
+    babelConfig.plugins.push(
+      require.resolve("babel-plugin-add-module-exports")
+    );
+  }
+  let stream = js.pipe(babel(babelConfig));
+  if (modules === false) {
+    stream = stream.pipe(
+      stripCode({
+        start_comment: "@remove-on-es-build-begin",
+        end_comment: "@remove-on-es-build-end"
+      })
+    );
+  }
+  return stream.pipe(
+    gulp.dest(modules === false ? `dist/${entry}/es` : `dist/${entry}/lib`)
+  );
 }
 
 function compile(modules) {
-    let error = 0;
+  let error = 0;
 
-    function check() {
-        if (error && !process.argv['ignore-error']) {
-            process.exit(1);
-        }
+  function check() {
+    if (error && !process.argv["ignore-error"]) {
+      process.exit(1);
     }
+  }
 
-    let newStreams = entrys.map((entry) => {
-        let source = [
-            `${rootDir}/${entry}/src/**/*.ts`,
-            `${rootDir}/${entry}/src/**/*.tsx`,
-            '!node_modules/**/*.*',
-            `!${rootDir}/${entry}/node_modules/**/*.*`,
-            'typings/**/*.d.ts',
-        ]
-        const tsResult = gulp.src(source).pipe(ts(tsConfig, {
-            error(e) {
-                tsDefaultReporter.error(e);
-                error = 1;
-            },
-            finish: tsDefaultReporter.finish,
-        }));
+  let newStreams = entrys.map(entry => {
+    let source = [
+      `${rootDir}/${entry}/src/**/*.ts`,
+      `${rootDir}/${entry}/src/**/*.tsx`,
+      "!node_modules/**/*.*",
+      `!${rootDir}/${entry}/node_modules/**/*.*`,
+      "typings/**/*.d.ts"
+    ];
+    const tsResult = gulp.src(source).pipe(
+      ts(tsConfig, {
+        error(e) {
+          tsDefaultReporter.error(e);
+          error = 1;
+        },
+        finish: tsDefaultReporter.finish
+      })
+    );
 
-        tsResult.on('finish', check);
-        tsResult.on('end', check);
-        return merge2([babelify(tsResult.js, modules, entry), tsResult.dts.pipe(gulp.dest(modules === false ? `dist/${entry}/es` : `dist/${entry}/lib`))]);
-    })
+    tsResult.on("finish", check);
+    tsResult.on("end", check);
+    return merge2([
+      babelify(tsResult.js, modules, entry),
+      tsResult.dts.pipe(
+        gulp.dest(modules === false ? `dist/${entry}/es` : `dist/${entry}/lib`)
+      )
+    ]);
+  });
 
-    return merge2(newStreams)
+  return merge2(newStreams);
 }
 
-gulp.task('compile', ['compile-with-es'], () => {
-    return compile();
+gulp.task("compile", ["compile-with-es"], () => {
+  return compile();
 });
 
-gulp.task('compile-with-es', ["tsc"], () => {
-    compile(false);
+gulp.task("compile-with-es", ["tsc"], () => {
+  compile(false);
 });
---
+++ packages/slate-dev-environment/src/index.ts
@@ -0,0 +1,114 @@
+import isBrowser from "is-in-browser";
+
+/**
+ * Browser matching rules.
+ *
+ * @type {Array}
+ */
+
+const BROWSER_RULES: [string, RegExp][] = [
+  ["edge", /Edge\/([0-9\._]+)/],
+  ["chrome", /(?!Chrom.*OPR)Chrom(?:e|ium)\/([0-9\.]+)(:?\s|$)/],
+  ["firefox", /Firefox\/([0-9\.]+)(?:\s|$)/],
+  ["opera", /Opera\/([0-9\.]+)(?:\s|$)/],
+  ["opera", /OPR\/([0-9\.]+)(:?\s|$)$/],
+  ["ie", /Trident\/7\.0.*rv\:([0-9\.]+)\).*Gecko$/],
+  ["ie", /MSIE\s([0-9\.]+);.*Trident\/[4-7].0/],
+  ["ie", /MSIE\s(7\.0)/],
+  ["android", /Android\s([0-9\.]+)/],
+  ["safari", /Version\/([0-9\._]+).*Safari/]
+];
+
+let browser;
+
+if (isBrowser) {
+  for (const [name, regexp] of BROWSER_RULES) {
+    if (regexp.test(window.navigator.userAgent)) {
+      browser = name;
+      break;
+    }
+  }
+}
+
+/**
+ * Operating system matching rules.
+ *
+ * @type {Array}
+ */
+
+const OS_RULES: [string, RegExp][] = [
+  ["ios", /os ([\.\_\d]+) like mac os/i], // must be before the macos rule
+  ["macos", /mac os x/i],
+  ["android", /android/i],
+  ["firefoxos", /mozilla\/[a-z\.\_\d]+ \((?:mobile)|(?:tablet)/i],
+  ["windows", /windows\s*(?:nt)?\s*([\.\_\d]+)/i]
+];
+
+let os;
+
+if (isBrowser) {
+  for (const [name, regexp] of OS_RULES) {
+    if (regexp.test(window.navigator.userAgent)) {
+      os = name;
+      break;
+    }
+  }
+}
+
+/**
+ * Feature matching rules.
+ *
+ * @type {Array}
+ */
+
+const FEATURE_RULES: [string, (w: any) => any][] = [
+  [
+    "inputeventslevel1",
+    (window: any) => {
+      // @ts-ignore
+      const event = window.InputEvent ? new InputEvent("input") : {};
+      const support = "inputType" in event;
+      return support;
+    }
+  ],
+  [
+    "inputeventslevel2",
+    (window: any) => {
+      const element = window.document.createElement("div");
+      element.contentEditable = true;
+      const support = "onbeforeinput" in element;
+      return support;
+    }
+  ]
+];
+
+const features: string[] = [];
+
+if (isBrowser) {
+  for (const [name, test] of FEATURE_RULES) {
+    if (test(window)) {
+      features.push(name);
+    }
+  }
+}
+
+/**
+ * Export.
+ *
+ * @type {Boolean}
+ */
+
+export const IS_CHROME = browser === "chrome";
+export const IS_OPERA = browser === "opera";
+export const IS_FIREFOX = browser === "firefox";
+export const IS_SAFARI = browser === "safari";
+export const IS_IE = browser === "ie";
+export const IS_EDGE = browser === "edge";
+
+export const IS_ANDROID = os === "android";
+export const IS_IOS = os === "ios";
+export const IS_MAC = os === "macos";
+export const IS_WINDOWS = os === "windows";
+
+export const HAS_INPUT_EVENTS_LEVEL_1 = features.includes("inputeventslevel1");
+export const HAS_INPUT_EVENTS_LEVEL_2 = features.includes("inputeventslevel2");
