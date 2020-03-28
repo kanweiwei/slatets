ac72965177cb2a1575f8a570c381fbd42d564d9b
kww <kwwnjujlc@sina.com>
2020-03-28 17:31:18 +0800

    refactor: add some weakmap

---
+++ packages/slate-react/src/utils/weak-maps.ts
@@ -0,0 +1,9 @@
+export const NODE_TO_INDEX = new WeakMap();
+
+export const NODE__TO_KEY = new WeakMap();
+
+export const NODE_TO_ELEMENT = new WeakMap();
+
+export const NODE_TO_PARENT = new WeakMap();
+
+export const EDITOR_TO_ELEMENT = new WeakMap();
